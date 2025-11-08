928a5148ce8d2ed149e6c4253dedf3df6c459d9c
ROSTYK <rooostik2016@gmail.com>
2025-11-09 00:55:05 +0200

    fuck1

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log("Rooostik start");
 console.log("Яна любить сосати пісюнчик ростіка кожну годину");
 console.log("Яна любить сосати пісюнчик ростіка кожну годину");
+
+  git config --global user.email "you@example.com"
+    git config --global user.email "you@example.com"
